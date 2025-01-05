3457992bfdc445eb6db238f03ae67c431ee9663c
Aiji Yamamura <j2yamimjr3i@gmail.com>
2025-01-05 18:03:20 +0900

    APIのIP変更

--- src/apis/GetMappings.js
+++ src/apis/GetMappings.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL; // ローカル開発時はlocalhost、デプロイ時はSecrets参照
 
 export const getCurrentSeason = async (setCurrentSeason) => {
   try {
